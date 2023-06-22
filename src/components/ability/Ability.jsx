2f1b2287e91765e02d474f91dc70c182c63eb897
isabelle_lemire_web <isabellelemire36@gmail.com>
2023-06-21 21:39:05 -0400

    placer les dossiers de component et les fichiers de base de structure avec le RAFCE de base

---
+++ src/components/ability/Ability.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Ability.css";
+
+const Ability = () => {
+  return (
+    <div>Ability</div>
+  );
+};
+
+export default Ability;
---
+++ src/components/text/Text.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Text.css";
+
+const Text = () => {
+  return (
+    <div>Text</div>
+  );
+};
+
+export default Text;
