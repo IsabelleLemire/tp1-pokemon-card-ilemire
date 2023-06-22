b6760b5abcb34d1547b6d4e63677a083d4cf5211
isabelle_lemire_web <isabellelemire36@gmail.com>
2023-06-21 21:42:58 -0400

    validation si ok avec App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import Ability from './components/ability/Ability'
+import Card from './components/card/Card'
+import Container from './components/container/Container'
+import Image from './components/image/Image'
+import Text from './components/text/Text'
+import Title from './components/title/Title'
 import './index.css'
+
 
 function App() {
 
   return (
     <>
-      
+      <Container></Container>
+      <Image></Image>
+      <Ability></Ability>
+      <Card></Card>
+      <Text></Text>
+      <Title></Title>
     </>
   )
 }
